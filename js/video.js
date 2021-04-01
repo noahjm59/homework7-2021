8cd0542cffa427f235f1ec3c0e3ee681bc9cf0f9
noahjm59 <57961136+noahjm59@users.noreply.github.com>
2021-04-01 09:27:11 -0700

    Update video.js

--- js/video.js
+++ js/video.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function() {
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+});
 
